6d6173d19546b432ee44c33a55b0d009f6c526a8
SinhueHerrera90 <116138705+SinhueHerrera90@users.noreply.github.com>
2023-08-17 05:35:04 +0000

    Added direct associations

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,6 @@
 ActiveRecord::Schema.define(version: 2023_08_17_005121) do
 
   # These are extensions that must be enabled in order to support this database
-  enable_extension "citext"
   enable_extension "plpgsql"
 
   create_table "comments", force: :cascade do |t|
@@ -29,7 +28,7 @@ ActiveRecord::Schema.define(version: 2023_08_17_005121) do
   create_table "follow_requests", force: :cascade do |t|
     t.bigint "recipient_id", null: false
     t.bigint "sender_id", null: false
-    t.string "status", default: "pending"
+    t.string "status"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["recipient_id"], name: "index_follow_requests_on_recipient_id"
@@ -57,21 +56,20 @@ ActiveRecord::Schema.define(version: 2023_08_17_005121) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.citext "email", default: "", null: false
+    t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.citext "username"
+    t.string "username"
     t.boolean "private"
-    t.integer "likes_count", default: 0
-    t.integer "comments_count", default: 0
+    t.integer "likes_count"
+    t.integer "comments_count"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "photos_count"
+    t.integer "photos_count", default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   add_foreign_key "comments", "photos"
