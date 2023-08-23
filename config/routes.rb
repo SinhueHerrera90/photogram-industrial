cdb51b9233698b79b6b5b775902c579e52e84991
SinhueHerrera90 <116138705+SinhueHerrera90@users.noreply.github.com>
2023-08-23 05:30:21 +0000

    Added root to page

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
+  
+  root "photos#index"
+
+  devise_for :users
+
   resources :likes
   resources :follow_requests
   resources :comments
   resources :photos
-  resources :users
-  devise_for :users
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
  
